3958d0eba82a9e0297b27ffb1a1446945d0b497a
sunminhong <akoda5555@gmail.com>
2025-04-29 23:21:27 +0900

    Initial Commit

---
+++ PropertyWrapper02/PropertyWrapper02App.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapper02App.swift
+//  PropertyWrapper02
+//
+//  Created by SunMin Hong on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapper02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
