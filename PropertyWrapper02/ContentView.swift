eb6e5c27c1153c63a5a60e4024c4347016f2ec65
sunminhong <akoda5555@gmail.com>
2025-04-30 11:02:20 +0900

    PostViewModel 적용 전

--- PropertyWrapper02/ContentView.swift
+++ PropertyWrapper02/ContentView.swift
@@ -9,16 +9,185 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            TabView {
+                Forum()
+                    .tabItem {
+                        Image(systemName: "bubble.right")
+                    }
+                Text("두번째 탭")
+                    .tabItem {
+                        Image(systemName: "house")
+                    }
+            }
+            .navigationTitle("M 스터디 방")
+        }
+    }
+}
+
+struct Forum: View {
+    @State private var list: [Post] = Post.list
+    @State private var showAddView: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(list) { post in
+                    NavigationLink {
+                        PostDetail(post: post)
+                    } label: {
+                        PostRow(post: post)
+                    }
+                    .tint(.primary)
+                }
+            }
+        }
+        .refreshable { }
+        .safeAreaInset(edge: .bottom, alignment: .trailing) {
+            Button {
+                showAddView = true
+            } label: {
+                Image(systemName: "plus")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Circle().fill(.white).shadow(radius: 4))
+            }
+            .padding()
+        }
+        .sheet(isPresented: $showAddView) {
+            PostAdd { post in
+                list.insert(post, at:0)
+            }
+        }
+    }
+}
+
+struct PostDetail: View {
+    @State private var showEditView: Bool = false
+    
+    let post: Post
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(post.username)
+            Text(post.content)
+                .font(.largeTitle)
+            Button {
+                showEditView = true
+            } label: {
+                Image(systemName: "pencil")
+                Text("수정")
+            }
+            .sheet(isPresented: $showEditView) {
+                
+            }
+        }
+    }
+}
+
+//class PostViewModel: ObservableObject {
+//    @Published var list: [Post] = Post.list
+//    
+//    func addPost(Text: String) {
+//        let newPost = Post(username: "유저이름", content: text)
+//        list.insert(newPost, at: 0)
+//    }
+//}
+
+struct PostAdd: View {
+    @FocusState private var focused: Bool
+    
+    //아래 추가 공부 필요
+    @Environment(\.dismiss) private var dismiss
+    @State private var text: String = ""
+    
+    //아래 추가 공부 필요
+    let action: (_ post: Post) -> ()
+    
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("포스트를 입력해 주세요..", text: $text)
+                    .font(.title)
+                    .padding()
+                    .padding(.top)
+                    .focused($focused)
+                    .onAppear { focused = true }
+                Spacer()
+            }
+            .navigationTitle("포스트 게시")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("취소") { dismiss()}
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("게시") {
+                        let newPost = Post(username: "유저이름", content: text)
+                        action(newPost)
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PostRow: View {
+    let post: Post
+    let colors: [Color] = [
+        Color.orange, Color.green, Color.pink, Color.blue, Color.yellow, Color.brown, Color.cyan, Color.purple
+    ]
+    var body: some View {
+        HStack {
+            Circle()
+                .fill(colors.randomElement() ?? . brown)
+                .frame(width: 30)
+            VStack(alignment: .leading) {
+                Text(post.username)
+                Text(post.content)
+                    .font(.title)
+            }
+            Spacer()
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .strokeBorder()
         }
         .padding()
     }
 }
 
+struct Post: Identifiable {
+    let id = UUID()
+    let username: String
+    let content:String
+}
+
+extension Post {
+    static var list: [Post] = [
+        Post(username: "나니", content: "나나냐"),
+        Post(username: "루니", content: "루루루니"),
+        Post(username: "호날두", content: "오호오호"),
+        Post(username: "쿠가", content: "쿠쿠다쿠"),
+        Post(username: "나나", content: "나나다"),
+        Post(username: "긴가", content: "긴가민가"),
+        Post(username: "헤호", content: "헤헤호호다")
+        
+    ]
+}
+
 #Preview {
+    //    ContentView()
+    //    PostRow(post: Post(username: "스티그", content: "헤이 안녕"))
+    //    PostDetail(post: Post(username: "스티그!", content: "헤이 안녕!"))
+    //    PostAdd()
+    //    PostAdd() { post in }
+//    NavigationView {
+//        Forum()
+//    }
     ContentView()
 }
